c7191ced71b86a3c148dbf6aa051ff9b6ec952aa
fullmoonissue <styvodiabolo@gmail.com>
2020-11-04 21:23:38 +0100

    Update the template when registering a file

--- assets/templates/new-tas-file.lua
+++ assets/templates/new-tas-file.lua
@@ -1,12 +1,23 @@
--- Don't hesitate to customize this new tas file template
+--[[
+
+>> File : assets/templates/new-tas-file.lua
+
+This file is the template when registering a new file (make register).
+
+# ### Example ### #
 
 local input = require('core/input')
---local macro = require('plugins/macro/collection')
 
-local cf = 1 -- "cf" stands for "Current Frame"
---[[cf = ]]input:start(cf, 2)
---print(cf)
---macro.example_macro_without_custom_inputs(1525)
---macro.example_macro_with_custom_inputs(3000, 7)
+local cf = 105 -- "cf" stands for "Current Frame"
+cf = input:start(cf, 12)
+print(cf)
 
+-- To use some macros (>> plugins/macro/collection.lua (example))
+local macro = require('plugins/macro/collection')
+macro.example_macro_without_custom_inputs(1525)
+macro.example_macro_with_custom_inputs(3000, 7)
+
+-- Awaited return
 return input:all()
+
+]]--
